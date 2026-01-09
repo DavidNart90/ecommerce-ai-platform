3797ec02dcd16d0c53111a72f2ca1023a825fd0d
DavidNart90 <davidnart90@gmail.com>
2026-01-09 23:59:40 +0000

    Update configuration and dependencies; enhance global styles and layout. Added support for remote images from Sanity, integrated new Sanity commands, and updated various Radix UI components. Modified global CSS for improved theming and removed the default home page.

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,13 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import { customerType } from './customerTypes'
+import { categoryType } from './categoryType'
+import { orderType } from './orderType'
+import { productType } from './productType'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [customerType, 
+    categoryType, 
+    orderType, 
+    productType,
+  ],
+}
