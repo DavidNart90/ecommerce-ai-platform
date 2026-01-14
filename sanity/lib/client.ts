5c68a48432f5ca703070579b22baa68f76b6859a
DavidNart90 <davidnart90@gmail.com>
2026-01-14 13:10:42 +0000

    Update package dependencies, including 'react-resizable-panels' to version 4.4.1 and add 'stripe' and 'tailwind-scrollbar-hide' packages. Enhance global styles by importing 'tailwind-scrollbar-hide' in CSS. Refactor resizable component to use updated imports from 'react-resizable-panels'. Introduce date formatting utility functions in utils.ts and add a write client for Sanity API mutations.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,15 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  perspective: 'published',
 })
+
+
+// Write Client (for Mutations - used in webhooks/ server actions)
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  token: process.env.SANITY_API_WRITE_TOKEN,
+  useCdn: false,
+});
